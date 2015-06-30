734c20d961bd06455fe9ca86f730dc23166bf8f6
Richard Smith <r.smith@jamesjones.co.uk>
2015-06-30 12:59:40 +0100

    Somewhat right, mostly wrong.  Can't be more specific yet as it needs fixing.

--- ArduAssist.ino
+++ ArduAssist.ino
@@ -1,4 +1,5 @@
 
+
 //Servo setup is as follows
 // Receiver
 //  Ch1 - Right stick vertical -	pitch
@@ -25,18 +26,17 @@
 #include <MPU6050_6Axis_MotionApps20.h>
 #include <MPU6050.h>
 #include <helper_3dmath.h>
+#include <Adafruit_SoftServo.h>
 
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
 
-#include "SoftwareServo.h"
 #include "HobbyRadioReceiver.h"
 #include "ServoMod.h"
 #include "ArduAssist.h"
 
 MPU6050 mpu;
-#define OUTPUT_READABLE_YAWPITCHROLL
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -48,19 +48,11 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
-
-SoftwareServo * servoArray;
+Adafruit_SoftServo * servoArray;
 int * pins;
 HobbyRadioReceiver * rec;
 
@@ -68,26 +60,71 @@ int iNumServos = 6;
 int iThrottle = 2;
 int ledPin = 13;
 bool ledOn = false;
+bool servoInit = false;
 
 unsigned long previousMillis    = 0;
 unsigned long previousMilli2    = 0;
 unsigned long calibrationMillis = 0;
+unsigned int toggle = 0;  //used to keep the state of the LED
+unsigned int count = 0;   //used to keep count of how many interrupts were fired
 
 ServoMod mod;
 
 int val;
 
+int iRollMapped = 90;
+int iPitchMapped = 90;
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+// ================================================================
+// ===               50ms SERVO REFRESH ROUTINE                ===
+// ================================================================
+
+//Timer2 Overflow Interrupt Vector, called every 1ms
+ISR(TIMER2_OVF_vect) {
+  if (servoInit)
+  {
+    count++;               //Increments the interrupt counter
+    if(count > 20){
+      toggle = !toggle;    //toggles the LED state
+      count = 0;           //Resets the interrupt counter
+      digitalWrite(ledPin,toggle);
+      for (int i=0; i< 6; i++)
+      {
+        servoArray[i].refresh();
+        //Serial.print(i);
+        //Serial.print(":");
+        //Serial.print(written[i]);
+      }
+      //Serial.println("---done---");
+    }
+  }
+  TCNT2 = 130;           //Reset Timer to 130 out of 255
+  TIFR2 = 0x00;          //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+};
+
+// ================================================================
+// ===                                                   Setup                                                        ===
+// ================================================================
 
 void setup()
-{  
+{
   pins = new int[3,5,6,9,10,11];
   
-  Serial.begin(9600);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  Serial.begin(115200);
+  //while (!Serial); // wait for Leonardo enumeration, others continue immediately
   // Specify the number of channels,
   //   followed by the pins the channels are attached to
   rec = new HobbyRadioReceiver( 6, A0, A1, A2, A3, A6, A7);
-  servoArray = new SoftwareServo[rec->getNumChannels()];
+  servoArray = new Adafruit_SoftServo[rec->getNumChannels()];
   
   
   pinMode(ledPin,OUTPUT);
@@ -104,6 +141,16 @@ void setup()
       servoArray[i].write(90);
     }
   }
+  
+  servoInit = true;
+  
+  //Setup Timer2 to fire every 1ms
+  TCCR2B = 0x00;        //Disbale Timer2 while we set it up
+  TCNT2  = 130;         //Reset Timer Count to 130 out of 255
+  TIFR2  = 0x00;        //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+  TIMSK2 = 0x01;        //Timer2 INT Reg: Timer2 Overflow Interrupt Enable
+  TCCR2A = 0x00;        //Timer2 Control Reg A: Wave Gen Mode normal
+  TCCR2B = 0x05;        //Timer2 Control Reg B: Timer Prescaler set to 128
   
   // join I2C bus (I2Cdev library doesn't do this automatically)
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -122,23 +169,32 @@ void setup()
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
   // wait for ready
-  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  while (Serial.available() && Serial.read()); // empty buffer
-  while (!Serial.available());                 // wait for data
-  while (Serial.available() && Serial.read()); // empty buffer again
+  //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  //while (Serial.available() && Serial.read()); // empty buffer
+  //while (!Serial.available());                 // wait for data
+  //while (Serial.available() && Serial.read()); // empty buffer again
 
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
+  //Uncomment for calibration to horizontal
   // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(17);
-  mpu.setYGyroOffset(-13);
-  mpu.setZGyroOffset(23);
   mpu.setXAccelOffset(-791);
   mpu.setYAccelOffset(-3500);
   mpu.setZAccelOffset(1494); // 1688 factory default for my test chip
+  mpu.setXGyroOffset(17);
+  mpu.setYGyroOffset(-13);
+  mpu.setZGyroOffset(23);
   
+  
+  //mpu.setXAccelOffset(-2680);
+  //mpu.setYAccelOffset(-3483);
+  //mpu.setZAccelOffset(3107); // 1688 factory default for my test chip
+  //mpu.setXGyroOffset(14);
+  //mpu.setYGyroOffset(-16);
+  //mpu.setZGyroOffset(26);
+
 
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
@@ -151,7 +207,7 @@ void setup()
     attachInterrupt(0, dmpDataReady, RISING);
     mpuIntStatus = mpu.getIntStatus();
 
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    //set our DMP Ready flag so the main loop() function knows it's okay to use it
     Serial.println(F("DMP ready! Waiting for first interrupt..."));
     dmpReady = true;
 
@@ -162,7 +218,7 @@ void setup()
     // 1 = initial memory load failed
     // 2 = DMP configuration updates failed
     // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(F("DMP Init failed (code "));
     Serial.print(devStatus);
     Serial.println(F(")"));
   }
@@ -171,23 +227,24 @@ void setup()
   Serial.println(rec->getNumChannels());
   
   Serial.println( "Starting calibration");
+  
+  for (int i = 1; i <= rec->getNumChannels(); i++ )
+  {
+    int j = i-1; //bloody indexing of the servo library - index starts at 1, not 0
+    int curVal = -255;
+    if (curVal < mod.servos[j].getMaxNeg())
+    {
+      mod.servos[j].setMaxNeg(curVal);
+    }
+    if (curVal > mod.servos[j].getMaxPos())
+    {
+      mod.servos[j].setMaxPos(curVal);
+    }
+  }
+  
   unsigned long calibrationStart = millis();
   while (millis() - calibrationStart < 10000) // first ten seconds for calibration.  Get max travel for all six raw channels coming from the transmitter
   {
-    unsigned long curCalibrationMillis = millis();
-    if (curCalibrationMillis - calibrationMillis > 250)
-    {
-      if (ledOn)
-      {
-        digitalWrite(ledPin,LOW);
-        ledOn = false;
-      }
-      else
-      {
-        digitalWrite(ledPin,HIGH);
-        ledOn = true;
-      }
-    }
     for (int i = 1; i <= rec->getNumChannels(); i++ )
     {
       int j = i-1; //bloody indexing of the servo library - index starts at 1, not 0
@@ -205,14 +262,14 @@ void setup()
   Serial.println( "Ending calibration");
   for (int i = 0; i< rec->getNumChannels(); i++)
   {
-    Serial.print("Max neg ch" );
+    Serial.print("Max neg ch " );
     Serial.print(i+1);
-    Serial.print(":");
+    Serial.print(" : ");
     Serial.print(mod.servos[i].getMaxNeg());
     Serial.println("");
-    Serial.print("Max pos ch" );
+    Serial.print("Max pos ch " );
     Serial.print(i+1);
-    Serial.print(":");
+    Serial.print(" : ");
     Serial.print(mod.servos[i].getMaxPos());
     Serial.println("");
   }
@@ -221,61 +278,57 @@ void setup()
   delay(5000);
 }
 
+// ================================================================
+// ===                                       Main program loop                                               ===
+// ================================================================
+
 void loop()
 {
-  unsigned long currentMillis = millis();
-
-  // here is where you'd put code that needs to be running all the time.
-  if(currentMillis - previousMilli2 > 50)
-  {
-    if (ledOn)
-    {
-      digitalWrite(ledPin,LOW);
-      ledOn = false;
-    }
-    else
-    {
-      digitalWrite(ledPin,HIGH);
-      ledOn = true;
-    }
-    previousMilli2 = currentMillis;
+  
+  //wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize)
+   {
+    // other program behavior stuff here
     for (int i = 1; i <= rec->getNumChannels(); i++ )
     {
       val = rec->check(i);
-      Serial.print(i);
-      Serial.print("=");
-      Serial.print( val );
-      Serial.print("\t");
-
-      
-      //      if (i == roll)
-      //      {
-      //        servoArray[leftAileron].write(mod.iLeftAileronSimple(val));
-      //        servoArray[rightAileron].write(mod.iRightAileronSimple(val));
-      //
-      //        int iLeft;
-      //        int iRight;
-      //        mod.AileronComplex(val,&iLeft,&iRight,90);
-      //        servoArray[leftAileron].write(iLeft);
-      //        servoArray[rightAileron].write(iRight);
-      //      }
-      //      else
-      //      {
       servoArray[i-1].write(mod.noChange(val,i-1));
-      //      }
     }
-    Serial.print("\n");
   }
 
-  // Servo(s) refresh only every 20 msec
-  if(currentMillis - previousMillis > 20)
-  {
-    previousMillis = currentMillis;
-    for (int i =0; i< iNumServos; i++)
-    {
-      servoArray[i].refresh();
-    }
-  }
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    
+    float fRoll = ypr[2] * 180/M_PI;
+    float fPitch = ypr[1] * 180/M_PI;
+    
+    iRollMapped = (int)constrain( map(fRoll,-90,90,0,180),0,180);
+    iPitchMapped = (int)constrain( map(fPitch,-90,90,0,180),0,180);
+  }  
 }
-
-
