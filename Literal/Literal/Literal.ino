3b66a10108dc03e3e7ce83b55a2618eb62d5497b
galorin <galorin@gmail.com>
2015-07-08 12:33:27 +0100

    Simplification and better comments

--- Literal/Literal/Literal.ino
+++ Literal/Literal/Literal.ino
@@ -1,32 +1,32 @@
-#include <HobbyRadioReceiver.h>
+#include <HobbyRadioReceiver.h> // see https://github.com/ScottCProjects/Arduino/blob/master/Libraries/HobbyRadioReceiver/HobbyRadioReceiver.h
 #include <Adafruit_SoftServo.h>
 
 /*
- * Literal.ino
- *
- * Created: 7/6/2015 3:54:03 PM
- * Author: richards
- */ 
+* Literal.ino
+*
+* Created: 7/6/2015 3:54:03 PM
+* Author: Richard Smith
+*/
 
+// only working with one channel and one servo right now.  Will eventually scale to six.
+//HobbyRadioReceiver rec( 2, A0, A1);
+//Adafruit_SoftServo servo1, servo2;
+HobbyRadioReceiver rec( 1, A0);
+Adafruit_SoftServo servo1;
 
-HobbyRadioReceiver rec( 2, A0, A1);
-Adafruit_SoftServo servo1, servo2;
+int ledPin = 13;          //Pin number for on-board LED.  Probably a #define somewhere for this.
+bool servoInit = false;   // Have we finished the servo initialization?
+bool toggle = false;      // Toggle for led on pin 13
+int count = 0;            // interrupt counter
+int val1Last = 90;        // Last value for servo1 from loop()
+int val2last = 90;        // Last value for servo2 from loop()
+int rx1MinMax[2] = {0,0}; // min and max values coming off radio receiver
 
-
-int iNumServos = 2;
-int ledPin = 13;
-bool servoInit = false;
-bool toggle = false;
-int count = 0;
-int val1Last = 90;
-int val2last = 90;
-int servo1MinMax[2] = {0,0};
-
-// ================================================================
+// =============================================
 // ===               50ms SERVO REFRESH ROUTINE                ===
-// ================================================================
+// =============================================
 
-//Timer2 Overflow Interrupt Vector, called every 1ms
+//Timer2 Overflow Interrupt, called every 1ms
 ISR(TIMER2_OVF_vect) {
   if (servoInit)
   {
@@ -41,14 +41,14 @@ ISR(TIMER2_OVF_vect) {
       servo1.refresh();
       
       Serial.print("\tch1Min: ");
-      Serial.print(servo1MinMax[0]);
+      Serial.print(rx1MinMax[0]);
       Serial.print("\tmax: ");
-      Serial.println(servo1MinMax[1]);
+      Serial.println(rx1MinMax[1]);
       
       //servo2.write(val2last);
       //Serial.print("\tch2: ");
       //Serial.println(val2last);
-      servo2.refresh();
+      //servo2.refresh();
     }
   }
   TCNT2 = 130;           //Reset Timer to 130 out of 255
@@ -59,48 +59,43 @@ void setup()
 {
   
   Serial.begin(115200);
-   //while (!Serial); // wait for Leonardo enumeration, others continue immediately
-   // Specify the number of channels,
-   //   followed by the pins the channels are attached to
-   
-servo1.attach(3);
-servo1.write(90);
-servo1.refresh();
+  
+  servo1.attach(3);
+  servo1.write(90);
+  servo1.refresh();
 
-servo2.attach(5);
-servo2.write(90);
-servo2.refresh();
-   
-   servoInit = true;
-   
-     //Setup Timer2 to fire every 1ms
-     TCCR2B = 0x00;        //Disbale Timer2 while we set it up
-     TCNT2  = 130;         //Reset Timer Count to 130 out of 255
-     TIFR2  = 0x00;        //Timer2 INT Flag Reg: Clear Timer Overflow Flag
-     TIMSK2 = 0x01;        //Timer2 INT Reg: Timer2 Overflow Interrupt Enable
-     TCCR2A = 0x00;        //Timer2 Control Reg A: Wave Gen Mode normal
-     TCCR2B = 0x05;        //Timer2 Control Reg B: Timer Prescaler set to 128
+  //servo2.attach(5);
+  //servo2.write(90);
+  //servo2.refresh();
+  
+  servoInit = true;
+  
+  //Setup Timer2 to fire every 1ms
+  TCCR2B = 0x00;        //Disbale Timer2 while we set it up
+  TCNT2  = 130;         //Reset Timer Count to 130 out of 255
+  TIFR2  = 0x00;        //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+  TIMSK2 = 0x01;        //Timer2 INT Reg: Timer2 Overflow Interrupt Enable
+  TCCR2A = 0x00;        //Timer2 Control Reg A: Wave Gen Mode normal
+  TCCR2B = 0x05;        //Timer2 Control Reg B: Timer Prescaler set to 128
 
 }
 
 void loop()
 {
 
-      int val1 = rec.check(1);
-      if (val1 < servo1MinMax[0])
-      {
-        servo1MinMax[0] = val1;
-      }
-      if (val1 > servo1MinMax[1])
-      {
-        servo1MinMax[1] = val1;
-      }
-      //int val1Translate = constrain(map(val1,servo1MinMax[0],servo1MinMax[1],0,179),0,180);
-      int val1Translate = constrain(map(val1,-255,255,0,179),0,180);
-      val1Last = val1Translate;
-      //servo1.write(valAvg);
-      
-
-	  /* add main program code here, this code starts again each time it ends */
-
+  int val1 = rec.check(1);
+  if (val1 < rx1MinMax[0])
+  {
+    rx1MinMax[0] = val1;
+  }
+  if (val1 > rx1MinMax[1])
+  {
+    rx1MinMax[1] = val1;
+  }
+  //Below line commented as we will stick to raw values for now.
+  //int val1Translate = constrain(map(val1,rx1MinMax[0],rx1MinMax[1],0,179),0,180);
+  int val1Translate = constrain(map(val1,-255,255,0,179),0,180);
+  
+  // do not write to the servo, that will be done in the interrupt loop.
+  val1Last = val1Translate;
 }
