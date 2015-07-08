1dcdc90d64e900e491f7882ff1271189cd15efca
Richard Smith <r.smith@jamesjones.co.uk>
2015-07-08 14:45:03 +0100

    Added in a rudimentary filter, and using my modifications to the HobbyRadioReceiver lib

--- Literal/Literal/Literal.ino
+++ Literal/Literal/Literal.ino
@@ -11,7 +11,7 @@
 // only working with one channel and one servo right now.  Will eventually scale to six.
 //HobbyRadioReceiver rec( 2, A0, A1);
 //Adafruit_SoftServo servo1, servo2;
-HobbyRadioReceiver rec( 1, A0);
+HobbyRadioReceiver rec( 1, A1);
 Adafruit_SoftServo servo1;
 
 int ledPin = 13;          //Pin number for on-board LED.  Probably a #define somewhere for this.
@@ -21,6 +21,7 @@ int count = 0;            // interrupt counter
 int val1Last = 90;        // Last value for servo1 from loop()
 int val2last = 90;        // Last value for servo2 from loop()
 int rx1MinMax[2] = {0,0}; // min and max values coming off radio receiver
+int skipCount = 0;
 
 // =============================================
 // ===               50ms SERVO REFRESH ROUTINE                ===
@@ -36,14 +37,14 @@ ISR(TIMER2_OVF_vect) {
       count = 0;           //Resets the interrupt counter
       digitalWrite(ledPin,toggle);
       servo1.write(val1Last);
-      Serial.print("ch1: ");
-      Serial.print(val1Last);
+      //Serial.print("ch1: ");
+      Serial.println(val1Last);
       servo1.refresh();
       
-      Serial.print("\tch1Min: ");
-      Serial.print(rx1MinMax[0]);
-      Serial.print("\tmax: ");
-      Serial.println(rx1MinMax[1]);
+      //Serial.print("\t\tch1Min: ");
+      //Serial.print(rx1MinMax[0]);
+      //Serial.print("\tmax: ");
+      //Serial.println(rx1MinMax[1]);
       
       //servo2.write(val2last);
       //Serial.print("\tch2: ");
@@ -69,6 +70,7 @@ void setup()
   //servo2.refresh();
   
   servoInit = true;
+  rec.setMode(1,"center");
   
   //Setup Timer2 to fire every 1ms
   TCCR2B = 0x00;        //Disbale Timer2 while we set it up
@@ -83,8 +85,9 @@ void setup()
 void loop()
 {
 
-  int val1 = rec.check(1);
-  if (val1 < rx1MinMax[0])
+  int val1 = rec.checkServo(1);
+  //Serial.println(val1);
+  if (val1 < rx1MinMax[0] && val1 >=0)
   {
     rx1MinMax[0] = val1;
   }
@@ -93,9 +96,25 @@ void loop()
     rx1MinMax[1] = val1;
   }
   //Below line commented as we will stick to raw values for now.
-  //int val1Translate = constrain(map(val1,rx1MinMax[0],rx1MinMax[1],0,179),0,180);
-  int val1Translate = constrain(map(val1,-255,255,0,179),0,180);
+  int val1Translate = constrain(map(val1,17,154,0,179),0,180);
+  //int val1Translate = constrain(map(val1,-255,255,0,179),0,180);
+  //int val1Translate = val1;
   
   // do not write to the servo, that will be done in the interrupt loop.
-  val1Last = val1Translate;
+  if (val1Last < val1Translate - 5 || val1Last > val1Translate + 5)
+  {
+    skipCount++;
+  }    
+  else
+  {
+    val1Last = val1Translate;    
+    skipCount = 0;
+  }
+  
+  if (skipCount > 3)
+  {
+    skipCount = 0;
+    val1Last = val1Translate;    
+  }
+  
 }
