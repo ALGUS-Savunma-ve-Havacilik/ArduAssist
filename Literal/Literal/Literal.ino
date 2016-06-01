3731056b909efd328ea46828298f50506d3593f0
galorin <galorin@gmail.com>
2016-06-01 14:33:46 +0100

    Edited literal.ino so as to read channels 1-5 and send formatted for serial plotter

--- Literal/Literal/Literal.ino
+++ Literal/Literal/Literal.ino
@@ -1,5 +1,5 @@
-#include <HobbyRadioReceiver.h> // see https://github.com/ScottCProjects/Arduino/blob/master/Libraries/HobbyRadioReceiver/HobbyRadioReceiver.h
-#include <Adafruit_SoftServo.h>
+#include "HobbyRadioReceiver.h" // see https://github.com/ScottCProjects/Arduino/blob/master/Libraries/HobbyRadioReceiver/HobbyRadioReceiver.h
+//#include <Adafruit_SoftServo.h>
 
 /*
 * Literal.ino
@@ -8,21 +8,19 @@
 * Author: Richard Smith
 */
 
-// only working with one channel and one servo right now.  Will eventually scale to six.
-//HobbyRadioReceiver rec( 2, A0, A1);
-//Adafruit_SoftServo servo1, servo2;
-
 #define STEP 8
-
+/*
 unsigned long previousMillis    = 0;
 unsigned long previousMilli2    = 0;
 bool bUP                        = true;
 int pos                         = 0;
+*/
 
-HobbyRadioReceiver rec( 1, A1);
+HobbyRadioReceiver rec( 5, A0, A1, A2, A3, A4, A5);
+/*
 Adafruit_SoftServo servo1;
 
-int ledPin = 13;          //Pin number for on-board LED.  Probably a #define somewhere for this.
+int ledPin = 13;          // Pin number for on-board LED.  Probably a #define somewhere for this.
 bool servoInit = false;   // Have we finished the servo initialization?
 bool toggle = false;      // Toggle for led on pin 13
 int count = 0;            // interrupt counter
@@ -38,9 +36,9 @@ int index = 0;                  // the index of the current reading
 int total = 0;                  // the running total
 int average = 0;                // the average
 
-// =============================================
-// ===               20ms SERVO REFRESH ROUTINE                ===
-// =============================================
+// ==================================
+// === 20ms SERVO REFRESH ROUTINE ===
+// ==================================
 
 //Timer2 Overflow Interrupt, called every 1ms
 ISR(TIMER2_OVF_vect) {
@@ -68,23 +66,28 @@ ISR(TIMER2_OVF_vect) {
   TCNT2 = 130;           //Reset Timer to 130 out of 255
   TIFR2 = 0x00;          //Timer2 INT Flag Reg: Clear Timer Overflow Flag
 };
-
+*/
 void setup()
 {
   
   Serial.begin(115200);
   
-  servo1.attach(3);
-  servo1.write(90);
-  servo1.refresh();
+  //servo1.attach(3);
+  //servo1.write(90);
+  //servo1.refresh();
 
   //servo2.attach(5);
   //servo2.write(90);
   //servo2.refresh();
   
-  servoInit = true;
+  //servoInit = true;
   rec.setMode(1,"center");
-  
+  rec.setMode(2,"center");
+  rec.setMode(3,"center");
+  rec.setMode(4,"center");
+  rec.setMode(5,"center");
+
+  /*
   //Setup Timer2 to fire every 1ms
   TCCR2B = 0x00;        //Disbale Timer2 while we set it up
   TCNT2  = 130;         //Reset Timer Count to 130 out of 255
@@ -95,47 +98,20 @@ void setup()
   
   //for (int thisReading = 0; thisReading < numReadings; thisReading++)
   //readings[thisReading] = 0;
+  */
 
 }
 
 void loop()
-{
-  //Below line commented as we will stick to raw values for now.
-  //int val1Translate = constrain(map(rec.checkServo(1),17,154,0,180),0,180);
-  //int val1Translate = constrain(map(val1,-255,255,0,179),0,180);
-  //int val1Translate = val1;
-  
-  //val1Translate = 90;
-  
-  unsigned long currentMillis = millis();
-
-  // here is where you'd put code that needs to be running all the time.
-  if(currentMillis - previousMilli2 > 50)
-  {
-    previousMilli2 = currentMillis;
-    if (bUP)
-    {
-      if (pos >=180)
-      {
-        bUP = false;
-        pos = 180;
-      }
-      else
-      {
-        pos += STEP;
-      }
-    }
-    else
-    {
-      if (pos <= 0)
-      {
-        bUP = true;
-        pos = 0;
-      }
-      else
-      {
-        pos -= STEP;
-      }
-    }
-  }
+{  
+  Serial.print(rec.check(1));
+  Serial.print(",");
+  Serial.print(rec.check(2));
+  Serial.print(",");
+  Serial.print(rec.check(3));
+  Serial.print(",");
+  Serial.print(rec.check(4));
+  Serial.print(",");
+  Serial.println(rec.check(5));
+  delay(100); 
 }
