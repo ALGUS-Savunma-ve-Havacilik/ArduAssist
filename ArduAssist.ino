a73cef467145790c662de5340a3dacf301eaaf0f
Sarah Smith <galorin@gmail.com>
2017-04-26 17:00:03 +0100

    Need correct values for mapping input to output to get more servo travel.

--- ArduAssist.ino
+++ ArduAssist.ino
@@ -21,9 +21,14 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOMIN  500 // this is the 'minimum' pulse length in ms
+#define SERVOMAX  2500 // this is the 'maximum' pulse length in ms
 
+#define MINPULSE 1250
+#define MAXPULSE 1650
+
+// 580 for 4 degrees 
+// 2380 for 176 degrees
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -33,7 +38,7 @@ uint8_t * readPins;
 void setup()
 {
   //chans = new uint8_t[0,1,3,4,5];
-  readPins = new uint8_t[A0,A1,A2,A3,A4];
+  readPins = new uint8_t[A0,A1,A2,A3,A6];
   for (int i = 0; i<sizeof(readPins) / sizeof(uint8_t); i++)
   {
     pinMode(readPins[i], INPUT);
@@ -49,6 +54,7 @@ void loop()
 	unsigned long duration = pulseIn(readPins[count], HIGH);
 	//int iDegrees = (int)constrain( map(fRoll,-90,90,0,180),0,180);
   //pulselength = map(degrees, 0, 180, SERVOMIN, SERVOMAX);
-    pwm.setPWM(count, 0, duration);
+  unsigned long mappedDur = map(duration, MINPULSE, MAXPULSE, SERVOMIN, SERVOMAX); 
+    pwm.setPWM(count, 0, mappedDur);
   }
 }
