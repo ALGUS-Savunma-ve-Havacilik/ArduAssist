ebd51e5c57419fdef6d2a078dc5875be328a26a4
Richard Smith <galorin@gmail.com>
2017-05-01 07:52:24 +0100

    Added in appropriate pulse lengths

--- ArduAssist.ino
+++ ArduAssist.ino
@@ -28,9 +28,9 @@
 
 //I worked out that the centre (90 degrees) was 1480 and calculated 
 //45 and 135 and it went right to them. Pity about upper and lower (0 and 180) not being available though.
-#define SERVOMIN 580 // this is the 'minimum' pulse length in ms
-#define SERVOMAX 2380 // this is the 'maximum' pulse length in ms
-#define SERVOMID 1480
+#define SERVOMIN 160 // this is the 'minimum' pulse length in ms
+#define SERVOMAX 580 // this is the 'maximum' pulse length in ms
+#define SERVOMID 370
 
 #define MINPULSE 1250 // Minimum pulse from receiver
 #define MAXPULSE 1650 // maximum pulse from receiver
@@ -44,7 +44,7 @@ uint8_t * readPins;
 void setup()
 {
   //chans = new uint8_t[0,1,3,4,5];
-  readPins = new uint8_t[A0];//,A1,A2,A3,A6];
+  readPins = new uint8_t[A0,A1,A2 ];//,A1,A2,A3,A6];
   for (int i = 0; i<sizeof(readPins) / sizeof(uint8_t); i++)
   {
     pinMode(readPins[i], INPUT);
@@ -61,11 +61,11 @@ void loop()
 {
   for (uint8_t count = 0; count < sizeof(readPins) / sizeof(uint8_t); count++)
   {
-	unsigned long duration = pulseIn(readPins[count], HIGH);
+	//unsigned long duration = pulseIn(readPins[count], HIGH);
 	//int iDegrees = (int)constrain( map(fRoll,-90,90,0,180),0,180);
   //pulselength = map(degrees, 0, 180, SERVOMIN, SERVOMAX);
   //unsigned long mappedDur = map(duration, MINPULSE, MAXPULSE, SERVOMIN, SERVOMAX); 
     //pwm.setPWM(count, 0, mappedDur);
-    pwm.setPWM(count, 0, duration);
+    pwm.setPWM(count, 0, SERVOMID);
   }
 }
