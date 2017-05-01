5c118a66bfb5886bca546a637bc97813061ac678
Sarah Smith <galorin@gmail.com>
2017-05-01 16:18:52 +0100

    Added in a few sketches for testing gyro.

---
+++ Euler-DMP/Euler-DMP.ino
@@ -0,0 +1,11 @@
+// Add libraries found in this folder.  Use the DMP6 example sketch to get Euler angles.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
