dcd113dd1df41d22484772a299dfbd80bd47fd9a
Sarah Smith <galorin@gmail.com>
2017-05-09 14:18:30 +0100

    Changing datatypes

--- ArduAssist.ino
+++ ArduAssist.ino
@@ -19,11 +19,11 @@
 //  PWM6 - unknown/unused
 
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>  bjm q 
+#include <Adafruit_PWMServoDriver.h>
 
-#define SERVOMIN 160 // this is the 'minimum' 60hz duration : 0 degrees+ 
-#define SERVOMAX 580 // this is the 'maximum' 60hz duration : 180 degrees
-#define SERVOMID 370 // This is the 'middle' 60hz duration : 90 degrees
+//#define SERVOMIN 160 // this is the 'minimum' 60hz duration : 0 degrees+ 
+//#define SERVOMAX 580 // this is the 'maximum' 60hz duration : 180 degrees
+//#define SERVOMID 370 // This is the 'middle' 60hz duration : 90 degrees
 
 #define MINPULSE 1140 // Minimum pulse from receiver
 #define MIDPULSE 1450 // Centered pulse from receiver
@@ -32,49 +32,70 @@
 // called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+class servo
+{
+  public:
+  uint16_t start = 160;
+  uint16_t midpt = 580;
+  uint16_t endpt = 370;
+};
+
+servo servos[15];
+
 //uint8_t * chans;
 uint8_t readPins[] = {A0, A1, A2, A3 } ;
 uint8_t aileronPins[] = {5, 6} ; // Put aileron servos individually on servo board 4 & 5 9ndex from 0
 
 void setup()
 {
-  for (int i = 0; i<sizeof(readPins) / sizeof(uint8_t); i++)
-  {
-    pinMode(readPins[i], INPUT);
-  }
+
+  uint8_t readSize =  sizeof(readPins) / sizeof(uint8_t);
+
+  servos[aileronPins[0] ].start = 265;
+  servos[aileronPins[0] ].midpt = 370;
+  servos[aileronPins[0] ].endpt = 475;
+
+  servos[aileronPins[1] ].start = 265;
+  servos[aileronPins[1] ].midpt = 370;
+  servos[aileronPins[1] ].endpt = 475;
+  
   pwm.begin();
   Serial.begin(9600);
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
-  for (uint8_t count = 0; count < sizeof(readPins) / sizeof(uint8_t); count++)
+  for (uint8_t count = readSize; count >=0 ; count--)
   {
+    pinMode(readPins[count], INPUT);
+    
     if (count == 1)
     {
-      pwm.setPWM(5, 0, SERVOMID);
-      pwm.setPWM(6, 0, SERVOMID);
+      pwm.setPWM(aileronPins[0], 0, servos[aileronPins[0]].midpt );
+      pwm.setPWM(aileronPins[1], 0, servos[aileronPins[1]].midpt);
     }
     else
     {
-      pwm.setPWM(count, 0, SERVOMID);
+      pwm.setPWM(count, 0, servos[count].midpt);
     }
   }  
 }
 
 void loop()
 {
+  uint8_t readSize =  sizeof(readPins) / sizeof(uint8_t);
   for (uint8_t count = 0; count < sizeof(readPins) / sizeof(uint8_t); count++)
   {
 	  unsigned long duration = pulseIn(readPins[count], HIGH);
     Serial.print(readPins[count]);
-    Serial.print(",");
+    Serial.print(F(","));
     Serial.println(duration);
-    int angle = map(duration,MINPULSE,MAXPULSE,-90,90);
+    //int angle = map(duration,MINPULSE,MAXPULSE,-90,90);
     
     //pulselength = constrain(map(degrees, 0, 180, SERVOMIN, SERVOMAX),SERVOMIN,SERVOMAX); // gets a pulse length for an angle
-    unsigned long mappedDur = constrain(map(duration, MINPULSE, MAXPULSE, SERVOMIN, SERVOMAX),SERVOMIN,SERVOMAX); 
+    //unsigned long mappedDur = constrain(map(duration, MINPULSE, MAXPULSE, SERVOMIN, SERVOMAX),SERVOMIN,SERVOMAX); 
+    uint16_t mappedDur = 1500;
     if (count == 1)
     {
-      pwm.setPWM(5, 0, mappedDur);
-      pwm.setPWM(6, 0, mappedDur);
+      pwm.setPWM(aileronPins[0], 0, mappedDur);
+      pwm.setPWM(aileronPins[1], 0, mappedDur);
     }
     else
     {
